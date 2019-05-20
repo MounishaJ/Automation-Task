946ad556806cea4746b079f2a80ee027a45211b2
mounisha <mounisha.resileo@gmail.com>
2019-05-20 18:41:48 +0530

    20/5/2019

--- src/LinkCount.java
+++ src/LinkCount.java
@@ -1,12 +1,14 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LinkCount {
 
-	public static void main(String[] args) {
-		// 5/20/2019
+	public static void main(String[] args) throws Exception
+	{
+		// 5/20/2019 monday
 		
 		System.setProperty("webdriver.chrome.driver", "F://Web Drivers/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
@@ -22,7 +24,15 @@ public class LinkCount {
 		WebElement columndriver=footerdriver.findElement(By.xpath("//table/tbody/tr/td[1]/ul"));
 		System.out.println(columndriver.findElements(By.tagName("a")).size());
 		
-
+// 4click on each link in the column and check if the pages or opening 
+		
+		for(int i=1;i<columndriver.findElements(By.tagName("a")).size();i++)
+		{
+			String clickonlinktab=Keys.chord(Keys.CONTROL,Keys.ENTER);
+			columndriver.findElements(By.tagName("a")).get(i).sendKeys(clickonlinktab);
+			Thread.sleep(2000);
+		}
+		
 	}
 
 }
